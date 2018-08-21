3d3842f78afb0a7ccdf02b2357d1a769a4ac7c4e
unknown <tecjuliohenrique@gmail.com>
2018-08-21 04:12:47 -0300

    ajuste

--- js/server.js
+++ js/server.js
@@ -4,6 +4,7 @@ server.deploy({
   port: 80,
   domain:'juliohenrique.me',
   https: {
+    ca: `./ca_bundle.crt`,
     privateKey: `./private.key`,
     certificate: `./certificate.crt`,
     port: 443,
