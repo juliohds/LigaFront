e575b7f15ab2c4f15050b3b32df8cebbdec9f916
unknown <tecjuliohenrique@gmail.com>
2018-08-21 03:43:14 -0300

    ajuste

--- js/server.js
+++ js/server.js
@@ -3,12 +3,12 @@ const server=require('node-http-server');
 const config=new server.Config;
 config.errors['404']    = 'These are not the files you are looking for...';
 config.contentType.mp4  = 'video/mp4';
-config.port             = 8005;
+config.port             = 80;
 config.verbose          = true;
 config.root             = '~/myApp/'
 config.https.privateKey = `./private.key`;
 config.https.certificate= `./certificate.crt`;
-config.https.port       = 4433;
+config.https.port       = 443;
 config.https.only       = true;
 
 server.deploy(config);
